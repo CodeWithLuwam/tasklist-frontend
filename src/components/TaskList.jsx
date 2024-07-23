4730a658cd546af50c31befeeec3906331d0793f
Luwam Ghebremicael <luwammikael@gmail.com>
2024-07-23 00:11:11 -0700

    movetoinprogress, .find() , make shallow copy, update inprogress

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -39,6 +39,38 @@ const TaskList = ({ tasks, deleteTask, toggleIsComplete, updateTask }) => {
     setDraggedTask(null);
     setTargetSection(null);
   };
+
+  // Function to move a task to "In Progress" state
+  const moveToInProgress = (taskId) => {
+    // Find the task with the given taskId in the tasks array
+    const taskToUpdate = tasks.find(task => task.id === taskId);
+
+    // If a task with the given id is found
+    if (taskToUpdate) {
+      // ...taskToUpdate creates a shallow copy of the original task
+      // isInProgress is set to true to move it to "In Progress"
+      // isComplete is set to false in case it was previously completed
+      // Create a new task object called updatedTask with updated properties
+      const updatedTask = { ...taskToUpdate, isInProgress: true, isComplete: false };
+
+      // Call the updateTask function (passed in as a prop from App.jsx) with the newly created updatedTask
+      // This will update the task in the parent component's state
+      updateTask(updatedTask);
+    }
+  };
+
+  // Function to move a task from "Complete" to "In Progress" state by pressing 'Pause'
+  const moveToPaused = (taskId) => {
+    const taskToUpdate = tasks.find(task => task.id === taskId);
+    if (taskToUpdate) {
+      const updatedTask = { ...taskToUpdate, isInProgress: true, isComplete: false };
+      updateTask(updatedTask);
+    }
+  };
+
+  // Filter tasks into appropriate categories
+  // These filtered arrays are used in the render method (renderTaskSection( 'To-do', todoTasks, 'todo'))
+  // Tasks that are neither in progress nor complete
   const todoTasks = tasks.filter(task => !task.isInProgress && !task.isComplete);
   const inProgressTasks = tasks.filter(task => task.isInProgress && !task.isComplete);
   const doneTasks = tasks.filter(task => task.isComplete);
